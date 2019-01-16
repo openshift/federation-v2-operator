b181da998cfa48130e2b9f5ccbc07c9fa5a21804
Paul Morie <pmorie@redhat.com>
2019-01-16 17:28:28 -0500

    Add missing test dependencies to stub

--- stub_test.go
+++ stub_test.go
@@ -1,9 +1,15 @@
 package main_test
 
+// The imports in this test stub exist to get dep to populate the dependencies
+// we need for the federation-v2 tests.
+
 import (
 	"testing"
 
 	_ "github.com/kubernetes-sigs/kubebuilder/pkg/test"
+	_ "github.com/onsi/ginkgo"
+	_ "github.com/onsi/gomega"
+	_ "github.com/stretchr/testify/assert"
 )
 
 func TestStub(t *testing.T) {
