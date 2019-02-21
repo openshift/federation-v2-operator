faab7a659cf4ee1bcf1d2245cb9f4cedc6484bc4
Paul Morie <pmorie@redhat.com>
2019-02-20 23:59:40 -0500

    Update package version to 0.0.5 in yaml

--- scripts/install-using-catalog-source.sh
+++ scripts/install-using-catalog-source.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# This script automates the work of installing federation using 
+# This script automates the work of installing federation using the
+# CatalogSource in the `olm-testing` directory.
+#
+# This script can be run from any directory.
 
-oc create -f olm-testing/catalog-source.yaml
-oc create -f olm-testing/operator-group.yaml
-oc create -f olm-testing/subscription.yaml
+dir=$(realpath "$(dirname "${BASH_SOURCE}")/..")
+
+oc create -f $dir/olm-testing/catalog-source.yaml
+oc create -f $dir/olm-testing/operator-group.yaml
+oc create -f $dir/olm-testing/subscription.yaml
