92968f286a10ccccf446bbdcd3dca5898e769f8d
Paul Morie <pmorie@redhat.com>
2019-04-12 13:04:23 -0400

    Add FLAVOR to scripts/populate-olm-crds.sh to control which manifest dir is populated

--- scripts/populate-olm-crds.sh
+++ scripts/populate-olm-crds.sh
@@ -16,7 +16,8 @@
 FEDERATION_CHART_DIR=vendor/github.com/kubernetes-sigs/federation-v2/charts/federation-v2/
 PACKAGE=${PACKAGE:-"federation"}
 VERSION=${VERSION:-"0.0.8"}
-MANIFESTS_DIR=manifests/${PACKAGE}/${VERSION}
+FLAVOR=${FLAVOR:-"manifests"}
+MANIFESTS_DIR=${FLAVOR}/${PACKAGE}/${VERSION}
 
 echo "Populating OLM manifests for package ${PACKAGE} version ${VERSION}"
 
