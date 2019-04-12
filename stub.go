89d4dc30077857db8f3e421431332f20dde8968b
Paul Morie <pmorie@redhat.com>
2019-04-12 08:48:29 -0400

    Prepare for 0.0.8 dep ensure

--- stub.go
+++ stub.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	. "github.com/kubernetes-sigs/federation-v2/cmd/hyperfed"
-	_ "github.com/kubernetes-sigs/federation-v2/pkg/client/clientset/versioned/fake"
 )
 
 // Note: at present, it is not expected that this file compile; we may change
